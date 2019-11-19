a0506ee26f5e5332c405ede75bb64cc74c66fef4
Jharell Chambers <jharell.chambers@selu.edu>
2019-11-19 00:53:18 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,4 +3,30 @@ require 'active_support/inflector'
 
 class InteractiveRecord
   
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end 
+  
+  def self.column_names
+    sql = "PRAGMA table_info('#{table_name}')"
+    
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+    
+    table_info.map do |column|
+      column_names << column["name"]
+    end 
+    
+    column_names.compact
+  end 
+  
+   def initialize(options={})
+    options.map do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
+  
+    def table_name_for_insert
+    self.class.table_name
+  end
 end
